beb76893734991715551abed85f507e0a306d644
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-09-09 10:03:44 +0530

    added main.cpp, main, license.txt

---
+++ Sublime_Setup/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int func(int);
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	// for getting input from input.txt
+	freopen("input1.txt", "r", stdin);
+
+	//for writing output to output.txt
+	freopen("output1.txt", "w", stdout);
+#endif
+	// int n;
+	// cin >> n;
+	// cout << n * n;
+
+	int T;
+	// scanf("%d", &T);
+	cin >> T;
+	while (T--)
+	{
+		int B;
+		// scanf("%d", &B);
+		// printf("%d\n", func(B));
+		cin >> B;
+		cout << func(B) << endl;
+	}
+	return 0;
+}
+
+int func(int n)
+{
+	if (n <= 1)
+		return 0;
+	else
+		return n / 2 + func(n - 2) - 1;
+}
