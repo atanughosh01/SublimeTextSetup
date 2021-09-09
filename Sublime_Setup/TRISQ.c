2229adc67e2d6ab6e3a25be131bba755200eeb1c
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-09-09 10:04:25 +0530

    added TRISQ.c

---
+++ Sublime_Setup/TRISQ.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int func(int n)
+{
+    if(n <= 1)
+        return 0;
+    else
+        return n/2 + func(n-2) - 1;
+}
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	while(T--)
+	{
+	    int B;
+	    scanf("%d", &B);
+        printf("%d\n", func(B));
+	}
+	return 0;
+}
