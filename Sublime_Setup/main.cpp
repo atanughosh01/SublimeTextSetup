f1966b1be3ff4c8df90f3f00f9ba6c03b3b8adb4
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-10-22 20:03:46 +0530

    modified template program

--- Sublime_Setup/main.cpp
+++ Sublime_Setup/main.cpp
@@ -1,39 +1,55 @@
-#include <iostream>
 #include <bits/stdc++.h>
+
+
+#define ll long  long
+#define ld long double
+#define mod 1000000007
+#define inf LLONG_MAX
+#define pb push_back
+#define ff first
+#define ss second
+#define all(x) x.begin(),x.end()
+#define FOR(i, a, b)    for(int i=a; i<b; i++)
+#define FORrev(i,a,b)  for(int i= a-1; i>=b; i--)
+#define vll vector <ll> 
+#define pll pair<ll, ll> 
+#define vpll vector <pll>
+#define input(a,n) FOR(i,0,n) cin>>a[i]
+#define mems(x, y) memset(x, y, sizeof(x))
+#define ThisIsDragonDen ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
 
-int func(int);
+//------------------------------------
 
-int main()
-{
-#ifndef ONLINE_JUDGE
-	// for getting input from input.txt
-	freopen("input1.txt", "r", stdin);
 
-	//for writing output to output.txt
-	freopen("output1.txt", "w", stdout);
-#endif
-	// int n;
-	// cin >> n;
-	// cout << n * n;
+const int MAX_N = 200000;
+int test;
 
-	int T;
-	cin >> T;
-	while (T != 0)
-	{
-		int B;
-		cin >> B;
-		cout << func(B) << endl;
-		T--;
-	}
-	return 0;
+void solve(){
+    // cout.precision(10);
+    // cout << fixed;
+    // priority_queue <int, vector<int>, greater<int> > pq;
+
+    ll n;
+    cin >> n;
+	cout << n*n << "\n";
 }
 
-int func(int n)
-{
-	if (n <= 1)
-		return 0;
-	else
-		return n / 2 + func(n - 2) - 1;
+int main(){
+    ThisIsDragonDen
+    #ifndef ONLINE_JUDGE
+        freopen("input1.txt", "r", stdin); 
+        freopen("output1.txt", "w", stdout); 
+    #else
+        //online submission
+    #endif
+    clock_t tStart = clock();
+    test = 1;
+    cin >> test;
+    FOR(i,1,test + 1){  
+        solve();
+    }
+    cerr << "Completed in: "<< (double)((clock() - tStart)*1000)/CLOCKS_PER_SEC<<" ms"<< endl;
+    return 0;
 }
