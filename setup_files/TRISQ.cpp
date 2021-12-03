55e22c513d3aabedb4789db3c93c675ab259c1e6
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-12-03 12:42:14 +0530

    Modified Source Code

--- setup_files/TRISQ.cpp
+++ setup_files/TRISQ.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+
+//------------------------------------
+
 #define ll long long
 #define ld long double
+#define lld long long double
+#define ull unsigned long long
 #define mod 1000000007
-#define inf LLONG_MAX
+#define mod1 998244353
+#define inf 1e18
 #define pb push_back
+#define ppb pop_back
+#define mp make_pair
 #define ff first
 #define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
 #define all(x) x.begin(),x.end()
 #define FOR(i, a, b)    for(int i=a; i<b; i++)
 #define FORrev(i,a,b)  for(int i= a-1; i>=b; i--)
@@ -14,10 +26,15 @@
 #define vpll vector <pll>
 #define input(a,n) FOR(i,0,n) cin>>a[i]
 #define mems(x, y) memset(x, y, sizeof(x))
-#define ThisIsDragonDen ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+//------------------------------------
 
 using namespace std;
+using namespace chrono;
 
+//------------------------------------
 //------------------------------------
 
 int func(int n)
@@ -29,14 +46,15 @@ int func(int n)
 }
 
 int main() {
-    ThisIsDragonDen
+    fastio();
 #ifndef ONLINE_JUDGE
     freopen("input1.txt", "r", stdin);
     freopen("output1.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
 #else
     //online submission
 #endif
-    clock_t tStart = clock();
+    auto start1 = high_resolution_clock::now();
     int T;
     cin >> T;
     while (T--)
@@ -45,6 +63,9 @@ int main() {
         cin >> B;
         cout << func(B) << endl;
     }
-    cerr << "Completed in: " << (double)((clock() - tStart) * 1000) / CLOCKS_PER_SEC << " ms" << endl;
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+    cerr << "Completed in: " << duration . count() << " microsec" << endl;
+    // cerr << "Completed in: " << (double)((clock() - tStart) * 1000) / CLOCKS_PER_SEC << " ms" << endl;
     return 0;
 }
