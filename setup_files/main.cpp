dbebbcfb82bb3d8978fb101c54c4a382b8a05fb3
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-12-03 21:25:54 +0530

    Modified Source Code

--- setup_files/main.cpp
+++ setup_files/main.cpp
@@ -56,7 +56,7 @@ int main() {
 #ifndef ONLINE_JUDGE
     freopen("input1.txt", "r", stdin);
     freopen("output1.txt", "w", stdout);
-    // freopen("error.txt", "w", stderr);
+    freopen("error.txt", "w", stderr);
 
 #else
     //online submission
--- setup_files/solve.cpp
+++ setup_files/solve.cpp
@@ -70,7 +70,7 @@ int main() {
 #ifndef ONLINE_JUDGE
 	freopen("input1.txt", "r", stdin);
 	freopen("output1.txt", "w", stdout);
-	// freopen("error.txt", "w", stderr);
+	freopen("error.txt", "w", stderr);
 
 #else
 	//online submission
